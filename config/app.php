06814d55f0dc0b4a7c5dbce977f43190ed5d367e
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2024-03-03 12:12:37 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -171,11 +171,13 @@ return [
          * Application Service Providers...
          */
         Mongodb\Laravel\MongodbServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
 
     ],
 
@@ -232,6 +234,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
